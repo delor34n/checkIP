932a10a98381506ed8d503cf530b5979682d9447
delor34n <sebastian.lastra@usach.cl>
2015-08-26 01:15:43 -0300

    add email delivery.

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ import requests, json
 import sys
 import sqlite3 as lite
 from datetime import datetime
+import configparser
 
-IP_URL = 'https://wtfismyip.com/json'
-PING_URL = 'http://www.google.cl'
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+import os
 
 def initDB():
 	try:
@@ -54,6 +58,55 @@ def insertIP(hostname, ip_address, location, isp, when_was_inserted):
 		if con:
 			con.close()
 
+def getLastIP():
+	try:
+		con = lite.connect('logs.db')
+		cur = con.cursor()
+		cur.execute("SELECT * FROM ip_log ORDER BY id DESC LIMIT 1")
+		rows = cur.fetchall()
+	except lite.Error as e:
+		if con:
+			con.rollback()
+		print("Error %s:" % e.args[0])
+		sys.exit(1)
+	finally:
+		if con:
+			con.close()
+		return rows
+
+def mail(to, subject, server_name, hostname, ip_address, location, isp, when_was_inserted):
+	msg = MIMEMultipart('alternative')
+
+	msg['From'] = server_name
+	msg['To'] = to
+	msg['Subject'] = subject
+
+	head = open ("head.html", "r")
+	html = head.read()
+	body = open ("email.html", "r")
+	html += body.read().format(hostname=hostname, ip_address=ip_address, location=location, isp=isp, when_was_inserted=when_was_inserted)
+
+	msg.attach(MIMEText(html, 'html'))
+
+	mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+	mailServer.ehlo()
+	mailServer.starttls()
+	mailServer.ehlo()
+	mailServer.login(GMAIL_USER, GMAIL_PASSWORD)
+	mailServer.sendmail(GMAIL_USER, to, msg.as_string())
+	mailServer.close()
+
+############################################################
+############################################################
+############################################################
+
+
+configParser = configparser.RawConfigParser()   
+configFilePath = r'email.cfg'
+configParser.read(configFilePath)
+
+IP_URL = configParser.get('URLS', 'IP_URL')
+PING_URL = configParser.get('URLS', 'PING_URL')
 response = requests.get(IP_URL)
 if response.status_code != 200:
 	testResponse = requests.get(PING_URL)
@@ -62,12 +115,44 @@ if response.status_code != 200:
 	sys.exit("404 - NOT FOUND")
 
 jsonResponse = response.json()
-print(jsonResponse['YourFuckingHostname'])
-print(jsonResponse['YourFuckingIPAddress'])
-print(jsonResponse['YourFuckingLocation'])
-print(jsonResponse['YourFuckingISP'])
 
-dropDB()
 initDB()
-insertIP(jsonResponse['YourFuckingHostname'], jsonResponse['YourFuckingIPAddress'],
-		jsonResponse['YourFuckingLocation'], jsonResponse['YourFuckingISP'], datetime.now())
+lastIP = getLastIP()
+if lastIP == []:
+	print("Inserting first IP address "+jsonResponse['YourFuckingIPAddress'])
+	insertIP(jsonResponse['YourFuckingHostname'], jsonResponse['YourFuckingIPAddress'],
+			 jsonResponse['YourFuckingLocation'], jsonResponse['YourFuckingISP'], datetime.now())
+	newIP = getLastIP()
+
+	GMAIL_USER = configParser.get('EMAIL', 'GMAIL_USER')
+	GMAIL_PASSWORD = configParser.get('EMAIL', 'GMAIL_PASSWORD')
+	SERVER_NAME = configParser.get('SERVER', 'SERVER_NAME')
+
+	mail(configParser.get('EMAIL', 'TO_EMAIL'),
+	   	 configParser.get('EMAIL', 'SUBJECT_EMAIL'),
+	   	 SERVER_NAME,
+	   	 newIP[0][1],
+	   	 newIP[0][2],
+	   	 newIP[0][3],
+	   	 newIP[0][4],
+	   	 newIP[0][5])
+elif lastIP[0][2] != jsonResponse['YourFuckingIPAddress']:
+	print("Inserting new IP address "+jsonResponse['YourFuckingIPAddress'])
+	insertIP(jsonResponse['YourFuckingHostname'], jsonResponse['YourFuckingIPAddress'],
+			 jsonResponse['YourFuckingLocation'], jsonResponse['YourFuckingISP'], datetime.now())
+	newIP = getLastIP()
+
+	GMAIL_USER = configParser.get('EMAIL', 'GMAIL_USER')
+	GMAIL_PASSWORD = configParser.get('EMAIL', 'GMAIL_PASSWORD')
+	SERVER_NAME = configParser.get('SERVER', 'SERVER_NAME')
+
+	mail(configParser.get('EMAIL', 'TO_EMAIL'),
+	   	 configParser.get('EMAIL', 'SUBJECT_EMAIL'),
+	   	 SERVER_NAME,
+	   	 newIP[0][1],
+	   	 newIP[0][2],
+	   	 newIP[0][3],
+	   	 newIP[0][4],
+	   	 newIP[0][5])
+else:
+	print("No IP changes")
